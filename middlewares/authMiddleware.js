3b17524357a5d134317f875d1c3521c1cfdf70c7
AshishManer <ashishmaner94@gmail.com>
2022-12-12 21:43:07 +0530

    first commit

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,26 +1,26 @@
-const jwt = require("jsonwebtoken");
-const User = require("../models/UserModel");
-const asyncHandler = require("express-async-handler");
+const jwt = require('jsonwebtoken')
+const User = require('../models/UserModel')
+const asyncHandler = require('express-async-handler')
 
 const protect = asyncHandler(async (req, res, next) => {
-  let token;
+  let token
   if (
     req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
+    req.headers.authorization.startsWith('Bearer')
   )
     try {
-      token = req.headers.authorization.split(" ")[1];
-      const decode = jwt.verify(token, process.env.JWT_KEY);
-      req.user = await User.findById(decode.id).select("-password");
-      next();
+      token = req.headers.authorization.split('')[1]
+      const decode = jwt.verify(token, process.env.JWT_KEY)
+      req.user = await User.findById(decode.id).select('-password')
+      next()
     } catch (error) {
-      console.error(error);
-      res.status(401);
-      throw new Error("Not Authorized , Token failed");
+      console.error(error)
+      res.status(401)
+      throw new Error('Not Authorized , Token failed')
     }
   if (!token) {
-    res.status(401);
-    throw new Error("Not Authorized, not token");
+    res.status(401)
+    throw new Error('Not Authorized, not token')
   }
-});
-module.exports = { protect };
+})
+module.exports = { protect }
